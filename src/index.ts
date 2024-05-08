376b00e1495bdae576b9264096680e30b48ac8af
van-eus <vanessauwase121@gmail.com>
2024-05-08 21:08:51 +0200

    Initial commit

--- src/index.ts
+++ src/index.ts
@@ -41,7 +41,7 @@ db.on("error", (error: Error) => {
 });
 
 // Connect to MongoDB using Mongoose
-mongoose.connect('mongodb://127.0.0.1:27017/RestAPIs?useNewUrlParser=true&useUnifiedTopology=true')
+mongoose.connect(' /RestAPIs?useNewUrlParser=true&useUnifiedTopology=true')
   .then(() => {
     console.log("Connected to MongoDB");
   })
